02f9894d6a568a12cbe04bba1041672e43352b2e
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-06-20 15:29:22 +0700

    refactor mail sending

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
 const MESG = require('mesg-js').service()
-const sendgrid = require('@sendgrid/mail')
-const fs = require('fs')
+const sendEmail = require('./sendEmail')
 
-const from = 'MESG Foundation <contact@mesg.tech>'
-const subject = 'Welcome to MESG'
-const text = fs.readFileSync('./email.txt').toString()
-
-const send = async ({ email, sendgridAPIKey }, { success, error }) => {
-  sendgrid.setApiKey(sendgridAPIKey)
-  try {
-    const [response, _] = await sendgrid.send({
-      to: email,
-      from,
-      subject,
-      text
-    })
-    success({
-      code: response.statusCode,
-      message: response.statusMessage
-    })
-  } catch(e) {
-    error({ message: e.toString() })
-  }
-}
+const send = ({ email, sendgridAPIKey }, { success, error }) =>
+  sendEmail(email, sendgridAPIKey)
+    .then(success)
+    .catch(e => error({ message: e.toString() }))
 
 MESG.listenTask({ send })
