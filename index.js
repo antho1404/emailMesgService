26b4b0d656ec3d8830f0ffa3dee5874a55a17c37
antho1404 <anthony.estebe@gmail.com>
2020-06-08 17:11:38 +0700

    migrate to Liteflow

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const MESG = require('mesg-js').service()
+const MESG = new (require('@liteflow/service'))()
 const sendEmail = require('./sendEmail')
 
 const send = ({ email, subject, messageBody, sendgridAPIKey }, { success, error }) =>
